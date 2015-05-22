eb0e4968b360d473da75cadcccb593c6709bb0de
Brendan Carlin <brendan.g.carlin@gmail.com>
2015-05-22 10:13:49 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+#***PROJECT 3 -- FROGGER ARCADE GAME CLONE***
+
+###HOW TO RUN THE GAME:
+
+1.) Select the index.html file from the following library ".../Brendan_Carlin-Project_3/index.html"
+2.) The game will load in your web browser
+
+###HOW TO PLAY THE GAME:
+
+Objective-- Your player needs to make it to the water without being hit by bugs!
+
+1.) Use the directional keypad to move your player.
+2.) Each arrow on your keypad corresponds with the appropriate player action.
+3.) Choose left to move left, right to move right, up for up, and down for down.
+4.) If your player touches a bug, you are infected and the game is over--the game will reset.
+5.) If your player makes it to the water you win.  The game will reset for you to win again!
---
+++ js/app.js
@@ -0,0 +1,105 @@
+// Enemies our player must avoid
+var Enemy = function(x, y) {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
+    this.speed = Math.floor((Math.random() * 250) + 150);
+};
+
+//set collision proximity for enemies
+var collisionProx = 25;
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+    //if the enemy crosses off screen, reset its position. Otherwise, it keeps running.
+    if (this.x <= 550){
+        this.x += this.speed * dt;
+    } else {
+        this.x = -5;
+    };
+
+    //If the player comes within 25px of an enemy's x and y coordinates, reset the game
+    if (player.y >= this.y - collisionProx && player.y <= this.y + collisionProx) {
+        if (player.x >= this.x - collisionProx && player.x <= this.x + collisionProx) {
+            alert("INFECTED! GAME OVER")
+            player.reset();
+        }
+    };
+};
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Create player class
+var Player = function() {
+    this.sprite = 'images/char-boy.png';
+    this.x = 200;
+    this.y = 400;
+};
+
+//Move player with keypad
+Player.prototype.update = function() {
+    if (this.ctlKey === 'left' && this.x > 15){
+        this.x = this.x - 101;
+    } else if (this.ctlKey === 'right' && this.x < 390){
+        this.x = this.x + 101;
+    } else if (this.ctlKey === 'up'){
+        this.y = this.y - 83;
+    } else if (this.ctlKey === 'down' && this.y != 400){
+        this.y = this.y + 83;
+    };
+    this.ctlKey = null;
+    if (this.y < 25) {
+        alert("You made it -- these bugs hate water! Congratulations!");
+        this.reset();
+    };
+};
+
+//Input handler for player object
+Player.prototype.handleInput = function(e){
+    this.ctlKey = e;
+};
+
+//Reset player to beginning position
+Object.prototype.reset = function() {
+    player.x = 200;
+    player.y = 400;
+};
+
+// Instantiate objects.
+// Place all enemy objects in an array called allEnemies
+var allEnemies = [];
+(function setEnemies(){
+    allEnemies.push(new Enemy(-2, 55));
+    allEnemies.push(new Enemy(-20, 225));
+    allEnemies.push(new Enemy(-10, 135));
+}());
+
+//create new Player object
+var player = new Player();
+
+//Add player to canvas
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
